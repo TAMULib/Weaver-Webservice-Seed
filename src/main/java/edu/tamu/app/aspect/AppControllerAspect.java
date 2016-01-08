78f6f8c7e7714454b83420e369a517ea79904be8
jeremythuff <jeremythuff@gmail.com>
2016-01-08 10:17:49 -0600

    init commit

---
+++ src/main/java/edu/tamu/app/aspect/AppControllerAspect.java
@@ -0,0 +1,13 @@
+package edu.tamu.app.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import edu.tamu.framework.aspect.CoreControllerAspect;
+
+
+@Component
+@Aspect
+public class AppControllerAspect extends CoreControllerAspect {
+
+}
---
+++ src/main/java/edu/tamu/app/controller/AdminController.java
@@ -0,0 +1,25 @@
+/* 
+ * AdminController.java 
+ * 
+ * Version: 
+ *     $Id$ 
+ * 
+ * Revisions: 
+ *     $Log$ 
+ */
+package edu.tamu.app.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** 
+ * Admin Controller.
+ * 
+ * @author
+ *
+ */
+@RestController
+@MessageMapping("/admin")
+public class AdminController {
+		
+}
